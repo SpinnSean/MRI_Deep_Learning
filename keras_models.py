8f376ac4773bb86955a65ac78de071d79af8e2fb
SpinnSean <sean.c.spinney@gmail.com>
2019-03-05 09:09:48 -0500

    Autoencoder 6/

--- keras_models.py
+++ keras_models.py
@@ -88,6 +88,47 @@ def cnn_binary_classifier(image_dim,verbose=1):
     return classifier
 
 
+def cnn_autoencoder_6(image_dim,verbose=1):
+    #encoder
+    #input = 28 x 28 x 1 (wide and thin)
+
+    padSize = int(image_dim[0] % 4 / 2)
+
+    input_img = Input(shape=(image_dim[0], image_dim[1], 1))
+    k = 3
+    #pad1 = ZeroPadding2D(padding=(padSize,padSize))(input_img)
+
+    conv1 = Conv2D(32, (k, k), activation='relu', padding='same')(input_img)
+    #conv1 = BatchNormalization()(conv1)
+    conv1 = Conv2D(64, (k, k), activation='relu', padding='same')(conv1) #28 x 28 x 32
+    #conv1 = BatchNormalization()(conv1)
+    conv2 = Conv2D(128, (k, k), activation='relu', padding='same')(conv1) #14 x 14 x 64
+    #conv2 = BatchNormalization()(conv2)
+    conv3 = Conv2D(128, (k, k), activation='relu', padding='same')(conv2) #7 x 7 x 128 (small and thick)
+    #conv3 = BatchNormalization()(conv3)
+    conv3 = Conv2D(256, (k, k), activation='relu', padding='same')(conv3)
+    #conv3 = BatchNormalization()(conv3)
+
+
+    #decoder
+    conv4 = Conv2D(128, (k, k), activation='relu', padding='same')(conv3) #7 x 7 x 128
+    #conv4 = BatchNormalization()(conv4)
+    conv4 = Conv2D(64, (k, k), activation='relu', padding='same')(conv4)
+    #conv4 = BatchNormalization()(conv4)
+    conv5 = Conv2D(32, (k, k), activation='relu', padding='same')(conv4) # 14 x 14 x 64
+    #conv5 = BatchNormalization()(convk)
+    conv5 = Conv2D(16, (k, k), activation='relu', padding='same')(conv5)
+    #conv5 = BatchNormalization()(conv5)
+
+    decoded = Conv2D(1, (k, k), activation='linear', padding='same')(conv5) # 28 x 28 x 1
+
+    autoencoder = Model(input_img, decoded)
+
+    if verbose > 0:
+        print(autoencoder.summary())
+
+    return autoencoder
+
 
 
 def cnn_autoencoder_5(image_dim,verbose=1):
@@ -97,32 +138,32 @@ def cnn_autoencoder_5(image_dim,verbose=1):
     padSize = int(image_dim[0] % 4 / 2)
 
     input_img = Input(shape=(image_dim[0], image_dim[1], 1))
-
+    k = 3
     #pad1 = ZeroPadding2D(padding=(padSize,padSize))(input_img)
 
-    conv1 = Conv2D(8, (5, 5), activation='relu', padding='same')(input_img)
-    conv1 = BatchNormalization()(conv1)
-    conv1 = Conv2D(16, (5, 5), activation='relu', padding='same')(conv1) #28 x 28 x 32
-    conv1 = BatchNormalization()(conv1)
-    conv2 = Conv2D(32, (5, 5), activation='relu', padding='same')(conv1) #14 x 14 x 64
-    conv2 = BatchNormalization()(conv2)
-    conv3 = Conv2D(64, (5, 5), activation='relu', padding='same')(conv2) #7 x 7 x 128 (small and thick)
-    conv3 = BatchNormalization()(conv3)
-    conv3 = Conv2D(128, (5, 5), activation='relu', padding='same')(conv3)
-    conv3 = BatchNormalization()(conv3)
+    conv1 = Conv2D(8, (k, k), activation='relu', padding='same')(input_img)
+    #conv1 = BatchNormalization()(conv1)
+    conv1 = Conv2D(16, (k, k), activation='relu', padding='same')(conv1) #28 x 28 x 32
+    #conv1 = BatchNormalization()(conv1)
+    conv2 = Conv2D(32, (k, k), activation='relu', padding='same')(conv1) #14 x 14 x 64
+    #conv2 = BatchNormalization()(conv2)
+    conv3 = Conv2D(128, (k, k), activation='relu', padding='same')(conv2) #7 x 7 x 128 (small and thick)
+    #conv3 = BatchNormalization()(conv3)
+    conv3 = Conv2D(128, (k, k), activation='relu', padding='same')(conv3)
+    #conv3 = BatchNormalization()(conv3)
 
 
     #decoder
-    conv4 = Conv2D(64, (5, 5), activation='relu', padding='same')(conv3) #7 x 7 x 128
-    conv4 = BatchNormalization()(conv4)
-    conv4 = Conv2D(32, (5, 5), activation='relu', padding='same')(conv4)
-    conv4 = BatchNormalization()(conv4)
-    conv5 = Conv2D(16, (5, 5), activation='relu', padding='same')(conv4) # 14 x 14 x 64
-    conv5 = BatchNormalization()(conv5)
-    conv5 = Conv2D(8, (5, 5), activation='relu', padding='same')(conv5)
-    conv5 = BatchNormalization()(conv5)
+    conv4 = Conv2D(64, (k, k), activation='relu', padding='same')(conv3) #7 x 7 x 128
+    #conv4 = BatchNormalization()(conv4)
+    conv4 = Conv2D(32, (k, k), activation='relu', padding='same')(conv4)
+    #conv4 = BatchNormalization()(conv4)
+    conv5 = Conv2D(16, (k, k), activation='relu', padding='same')(conv4) # 14 x 14 x 64
+    #conv5 = BatchNormalization()(convk)
+    conv5 = Conv2D(8, (k, k), activation='relu', padding='same')(conv5)
+    #conv5 = BatchNormalization()(conv5)
 
-    decoded = Conv2D(1, (5, 5), activation='sigmoid', padding='same')(conv5) # 28 x 28 x 1
+    decoded = Conv2D(1, (k, k), activation='linear', padding='same')(conv5) # 28 x 28 x 1
 
     autoencoder = Model(input_img, decoded)
 
@@ -142,34 +183,34 @@ def cnn_autoencoder_4(image_dim,verbose=1):
 
     pad1 = ZeroPadding2D(padding=(padSize,padSize))(input_img)
 
-    conv1 = Conv2D(64, (3, 3), activation='relu', padding='same')(pad1) #28 x 28 x 32
-    conv1 = BatchNormalization()(conv1)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(pad1) #28 x 28 x 32
+    #conv1 = BatchNormalization()(conv1)
     conv1 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv1)
-    conv1 = BatchNormalization()(conv1)
+    #conv1 = BatchNormalization()(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1) #14 x 14 x 32
     conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1) #14 x 14 x 64
-    conv2 = BatchNormalization()(conv2)
+    #conv2 = BatchNormalization()(conv2)
     conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
-    conv2 = BatchNormalization()(conv2)
+    #conv2 = BatchNormalization()(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2) #7 x 7 x 64
     conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2) #7 x 7 x 128 (small and thick)
-    conv3 = BatchNormalization()(conv3)
+    #conv3 = BatchNormalization()(conv3)
     conv3 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv3)
-    conv3 = BatchNormalization()(conv3)
+    #conv3 = BatchNormalization()(conv3)
 
 
     #decoder
     conv4 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv3) #7 x 7 x 128
-    conv4 = BatchNormalization()(conv4)
+    #conv4 = BatchNormalization()(conv4)
     conv5 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv4)
-    conv5 = BatchNormalization()(conv5)
+    #conv5 = BatchNormalization()(conv5)
     up1 = UpSampling2D((2,2))(conv5) # 14 x 14 x 128
     conv6 = Conv2D(32, (3, 3), activation='relu', padding='same')(up1) # 14 x 14 x 64
-    conv6 = BatchNormalization()(conv6)
+    #conv6 = BatchNormalization()(conv6)
     conv7 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv6)
-    conv7 = BatchNormalization()(conv7)
+    #conv7 = BatchNormalization()(conv7)
     conv8 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv7)
-    conv8 = BatchNormalization()(conv8)
+    #conv8 = BatchNormalization()(conv8)
     up2 = UpSampling2D((2,2))(conv8) # 28 x 28 x 64
     crop1 = Cropping2D(cropping=(padSize,padSize))(up2)
     conv9 = Conv2D(64, (3, 3), activation='relu', padding='same')(crop1)
@@ -465,6 +506,8 @@ def build_model(image_dim, nlabels,nK, n_dil, kernel_size, drop_out, model_type,
        model = cnn_autoencoder_4(image_dim)
     elif model_type == 'cnn-autoencoder-5':
        model = cnn_autoencoder_5(image_dim)
+    elif model_type == 'cnn-autoencoder-6':
+       model = cnn_autoencoder_6(image_dim)
     elif model_type == 'cnn-binary-classifier':
        model = cnn_binary_classifier(image_dim)
     elif model_type == 'cnn-binary-classifier-2':
@@ -477,7 +520,7 @@ def build_model(image_dim, nlabels,nK, n_dil, kernel_size, drop_out, model_type,
 def compile_and_run(target_dir, model, model_name, model_type, history_fn, X_train,  Y_train, X_validate, Y_validate,  nb_epoch, batch_size, nlabels, loss, verbose=0, metric="accuracy", lr=0.005, nGPU=1):
 
     #set compiler
-    ada = keras.optimizers.Adam(0.0001)
+    ada = keras.optimizers.Adam(0.01)
 
     #set checkpoint filename
     checkpoint_fn = str(os.path.join(target_dir, 'model', str(os.path.basename(model_name).split('.')[0]) +"_checkpoint-{epoch:02d}-{val_loss:.2f}.hdf5"))
@@ -499,7 +542,7 @@ def compile_and_run(target_dir, model, model_name, model_type, history_fn, X_tra
 
     if 'autoencoder' in model_type:
 
-        model.compile(loss=loss, optimizer=Adam())
+        model.compile(loss=loss, optimizer=Adam(0.01))
 
     elif 'classifier' in model_type:
 
@@ -510,10 +553,10 @@ def compile_and_run(target_dir, model, model_name, model_type, history_fn, X_tra
 
     # train model
     # augmentation generator
-    aug = ImageDataGenerator(rotation_range=10,
+    aug = ImageDataGenerator(rotation_range=1,
                              #width_shift_range=0.01,
                              #height_shift_range=0.01,
-                             zoom_range=0.3,
+                             zoom_range=0.001,
                              fill_mode="nearest")
     #if nGPU > 1:
     if 'autoencoder' in model_type:
